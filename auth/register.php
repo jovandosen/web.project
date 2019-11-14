497b6c5c46d6046b53e15eaeefda29e3afb0ad6f
jovandosen <jovandosen1994@gmail.com>
2019-11-14 18:13:40 +0100

    user auth files added

---
+++ auth/register.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Register</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="icon" href="/../assets/images/image_test.jpg">
+		<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="/../assets/css/index.css">
+		<link rel="stylesheet" type="text/css" href="/../assets/css/register.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	</head>
+	<body>
+
+		<div id="navigation">
+			<ul>
+				<li class="home">
+					<a href="/../index.php">
+						<div class="home-image">
+							<i class="fa fa-home"></i>
+						</div>
+					</a>
+				</li>
+				<li><a href="register.php">REGISTER</a></li>
+				<li><a href="#">LOGIN</a></li>
+			</ul>
+		</div>
+
+		<script src="/../assets/js/register.js"></script>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Index</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="icon" href="/assets/images/image_test.jpg">
+		<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="/assets/css/index.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	</head>
+	<body>
+
+		<div id="navigation">
+			<ul>
+				<li class="home">
+					<a href="index.php">
+						<div class="home-image">
+							<i class="fa fa-home"></i>
+						</div>
+					</a>
+				</li>
+				<li><a href="/auth/register.php">REGISTER</a></li>
+				<li><a href="#">LOGIN</a></li>
+			</ul>
+		</div>
+
+		<script src="/assets/js/index.js"></script>
+	</body>
+</html>
